fb06766d951770e48a4519d57d6a70ceb9d13515
Murilo Ferreira <106560525+MurFerDev@users.noreply.github.com>
2024-11-06 22:03:00 -0300

    Exercícios de C#
    Exercícios de C# realizados no Visual Studio para estudo de Programação Orientada a Objetos.

---
+++ Condicionais - Jogo de Aventura/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais___Jogo_de_Aventura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vida = 100, acao, escolha;
+            bool espada = false;
+
+            Console.WriteLine("Você encontrou uma espada. \nEscoha uma opção: \nPressione 1 -> Pegar espada \nPressione 2 -> Ignorar espada ");
+            escolha = Convert.ToInt32(Console.ReadLine());
+
+            if(escolha == 1)
+            {
+                espada = true;
+            }
+            else
+            {
+                espada = false;
+            }
+
+            Console.WriteLine("Você encontrou um monstro! \nEscoha uma ação: \nPressione 1 -> Atacar \nPressione 2 -> Fugir ");
+            acao = Convert.ToInt32(Console.ReadLine());
+
+            if (acao == 1)
+            {
+                if (espada == true)
+                {
+                    Console.WriteLine($"Você atacou o monstro. \nPontos de vida: {vida}");
+                }
+                else
+                {
+                    vida -= 30;
+                    Console.WriteLine($"Você atacou o monstro e perdeu 30 pontos de vida. \nPontos de vida: {vida}");
+                }
+            }
+
+            if(acao == 2)
+            {
+                Console.WriteLine($"Você fugiu do monstro. \nPontos de vida: {vida}");
+            }
+            //else
+            //{
+            //    Console.WriteLine($"Opção inválida, tente novamente!");
+            //}
+
+            Console.ReadKey(); 
+            
+        }
+    }
+}
---
+++ Condicionais/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*bool chovendo = false;
+
+            if (chovendo)
+            {
+                Console.WriteLine("Não esqueça o guarda-chuva");
+            }
+            else {
+                Console.WriteLine("Não esqueça o óculos de sol");
+            }*/ // 1º exemplo
+
+            /*int n1 = 1, n2 = 10;
+
+            if (n1 >= n2)
+            {
+                if(n1 == n2)
+                {
+                    Console.WriteLine("Os números são iguais");
+                }
+                Console.WriteLine($"O número {n1} é maior que {n2}");
+            }
+            else
+            {
+                Console.WriteLine($"O número {n2} é maior que {n1}");
+            }*/ // 2º exemplo
+
+
+            int n1, n2;
+
+            Console.WriteLine("Digite um número");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite um segundo número");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 >= n2)
+            {
+                if (n1 == n2)
+                {
+                    Console.WriteLine("Os números são iguais");
+                }
+                else
+                {
+                    Console.WriteLine($"O número {n1} é maior que {n2}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"O número {n2} é maior que {n1}");
+            }
+            
+
+            Console.ReadKey(); 
+        }
+    }
+}
