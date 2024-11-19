59f105b0e743bc60af361175683706bdfe341eae
Murilo Ferreira <106560525+MurFerDev@users.noreply.github.com>
2024-11-18 22:11:58 -0300

    Exercícios de C#
    Exercícios de C# realizados no Visual Studio para estudo de Programação Orientada a Objetos.

---
+++ Vetores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // inicialização com valores
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7 };
+
+            // inicialização de um valor inteiro com 7 posições
+            int[] notas = new int[7];
+
+            Console.WriteLine(numeros[3]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
