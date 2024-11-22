ede4944dcb445fd8e4a69539adf101c2b4ee6ffb
Murilo Ferreira <106560525+MurFerDev@users.noreply.github.com>
2024-11-21 22:10:40 -0300

    Exercícios de C#
    Exercícios de C# realizados no Visual Studio para estudo de Programação Orientada a Objetos.

---
+++ Exercicio_MetodosFuncoes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_MetodosFuncoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExibirMenu();
+
+            Console.Write("Escolha uma opção: ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch(opcao)
+            {
+                case 1:
+                    int qtd = ObterQuantidade();
+                    double valor = CalcularHamburger(qtd);
+                    Console.Write("Total: R$ " + valor);
+                    break;
+                case 2:
+                    qtd = ObterQuantidade();
+                    valor = CalcularBatata(qtd);
+                    Console.Write("Total: R$ " + valor);
+                    break;
+                case 3:
+                    qtd = ObterQuantidade();
+                    valor = CalcularRefrigerante(qtd);
+                    Console.Write("Total: R$ " + valor);
+                    break;
+                case 4:
+                    qtd = ObterQuantidade();
+                    valor = CalcularSobremesa(qtd);
+                    Console.Write("Total: R$ " + valor);
+                    break;
+                default:
+                    Console.Write("");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ExibirMenu()
+        {
+            Console.WriteLine("Confira nosso cardápio:");
+            Console.WriteLine("1 - Hamburger    (R$ 25,00)");
+            Console.WriteLine("2 - Batata Frita (R$ 7,00)");
+            Console.WriteLine("3 - Refrigerante (R$ 8,00)");
+            Console.WriteLine("4 - Sobremesa    (R$ 14,00)");
+        }
+
+        static int ObterQuantidade()
+        {
+            Console.Write("Digite a quantidade: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static double CalcularHamburger(int quantidade)
+        { return quantidade * 25.00; }
+
+        static double CalcularBatata(int quantidade)
+        { return quantidade * 7.00; }
+
+        static double CalcularRefrigerante(int quantidade)
+        { return quantidade * 8.00; }
+
+        static double CalcularSobremesa(int quantidade)
+        { return quantidade * 14.00; }
+
+    }
+}
---
+++ MetodosFuncoes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosFuncoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escolha uma operação: ");
+            Console.WriteLine("1 -> Soma (+)\n2 -> Subtração (-)\n3 -> Multiplicação (*)\n4 -> Divisão (/)");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Você escolheu Soma");
+                    Console.WriteLine("O resultado da soma é: " + Somar(n1,n2));
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu Subtrair");
+                    Console.WriteLine("O resultado da subtração é: " + Subtrair(n1, n2));
+                    break;
+                case 3:
+                    Console.WriteLine("Você escolheu Multiplicar");
+                    Console.WriteLine("O resultado da multiplicação é: " + Multiplicar(n1, n2));
+                    break;
+                case 4:
+                    Console.WriteLine("Você escolheu Dividir");
+                        Console.WriteLine("O resultado da divisão é: " + Dividir(n1, n2));
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        static int Somar(int a, int b)
+        {  return a + b; }
+
+        static int Subtrair(int a, int b)
+        { return a - b; }
+
+        static int Multiplicar(int a, int b)
+        { return a * b; }
+
+        static double Dividir(int a, int b)
+        {
+            if (b != 0)
+            {
+                return (double)a / b;
+            }
+            else
+            {
+                Console.WriteLine("Erro: Divisão por zero");
+                return 0;
+            }
+        }
+    }
+}
