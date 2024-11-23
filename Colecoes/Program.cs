8858d7c47ccc2bb7fa7a43757b1373eda2d5c164
Murilo Ferreira <106560525+MurFerDev@users.noreply.github.com>
2024-11-22 22:16:29 -0300

    Exercícios de C#
    Exercícios de C# realizados no Visual Studio para estudo de Programação Orientada a Objetos.

---
+++ Colecoes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* // *** EXEMPLO DE LISTA *** 
+            List<string> nomes = new List<string>();
+
+            nomes.Add("Cecília");
+            nomes.Add("Alice");
+            nomes.Add("Mariana");
+
+            Console.WriteLine("Nomes na lista: ");
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+            */
+
+            /* // *** EXEMPLO DE COLEÇÃO ***
+            HashSet<int> numeros = new HashSet<int>();
+
+            numeros.Add(10);
+            numeros.Add(20);
+            numeros.Add(10);
+
+            Console.WriteLine("Números no conjunto: ");
+
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+            */
+
+            // *** EXEMPLO DE MAPA / DICIONÁRIO ***
+            Dictionary<string, int> idades = new Dictionary<string, int>();
+
+            idades.Add("Enrico", 5);
+            idades.Add("Cecília", 5);
+            idades.Add("Alice", 3);
+            idades.Add("Mariana", 2);
+
+            Console.WriteLine("Idades: ");
+
+            foreach (var par in idades)
+            {
+                Console.WriteLine($"Nome: {par.Key}, {par.Value} anos");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ExercicioListaDeTarefas/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExercicioListaDeTarefas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            Console.WriteLine("Escolha uma das opções: \n1: Adicionar tarefa\n2: Remover tarefa\n3: Listar todas as tarefas\n4: Sair");
+            int opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Digite uma nova tarefa: ");
+                    lista.Add(Console.ReadLine());
+
+                    Console.WriteLine("Deseja adicionar outra tarefa?: 1 - Sim \n2 - Não");
+                    int novaTarefa = int.Parse(Console.ReadLine());
+
+                    if (novaTarefa == 1)
+                    {
+                        do
+                        {
+                            Console.WriteLine("Digite a tarefa a ser removida: ");
+                            lista.Add(Console.ReadLine());
+                        }
+                        while (novaTarefa == 1);
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("Digite a tarefa que deseja remover: ");
+                    lista.Remove(Console.ReadLine());
+                    break;
+
+                case 3:
+                    Console.WriteLine("Lista de tarefas:");
+
+                    foreach (var tarefa in lista)
+                    {
+                        Console.WriteLine(lista + "\n");
+                    }
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Saindo da lista de tarefas");
+                    Thread.Sleep(400);
+                    Console.Clear();
+                    Console.WriteLine("Saindo da lista de tarefas .");
+                    Thread.Sleep(400);
+                    Console.Clear();
+                    Console.WriteLine("Saindo da lista de tarefas ..");
+                    Thread.Sleep(400);
+                    Console.Clear();
+                    Console.WriteLine("Saindo da lista de tarefas ...");
+                    Thread.Sleep(400);
+                    Console.Clear();
+                    break;
+                default:
+                    Console.Write("Opção inválida!");
+                    break;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
