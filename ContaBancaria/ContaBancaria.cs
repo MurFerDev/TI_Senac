5d08a0b12e3e642b601476d8657dcc98bbba0465
Murilo Ferreira <106560525+MurFerDev@users.noreply.github.com>
2024-11-29 22:09:48 -0300

    Exercícios de C#
    Exercícios de C# realizados no Visual Studio para estudo de Programação Orientada a Objetos.

---
+++ ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class ContaBancaria
+    {
+        // Propriedades da conta
+        public string Titutlar { get; private set; }
+
+        private decimal _saldo;
+
+        // Construtor da conta
+        public ContaBancaria(string titutlar, decimal saldoinicial)
+        {
+            Titutlar = titutlar;
+            _saldo = saldoinicial;
+        }
+
+        // Métodos da conta
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"\nSaldo atual: R$ {_saldo:F2}");
+        }
+
+        public void Depositar(decimal valor)
+        {
+            _saldo += valor;
+            Console.WriteLine($"\nR$ {valor:F2} depositado com sucesso!");
+            ExibirSaldo();
+        }
+
+        public void Sacar(decimal retirada)
+        {
+            if (retirada > _saldo)
+            {
+                Console.WriteLine($"\nSaldo insuficiente!\nValor solicitado é maior que o saldo disponível.");
+            }
+            else if (retirada == 0)
+            {
+                Console.WriteLine("\nNenhum valor foi digitado. Operação cancelada.");
+            }
+            else
+            {
+                _saldo -= retirada;
+                Console.WriteLine($"\nSaque efetuado com sucesso! Você sacou R$ {retirada:F2}.");
+                ExibirSaldo();
+            }
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando uma conta bancária
+            ContaBancaria conta = new ContaBancaria("Jefferson José Jeremeias", 1000.00m);
+
+            // Exibindo informações inciais
+            Console.WriteLine($"Bem vindo(a), {conta.Titutlar}");
+
+            conta.ExibirSaldo();
+
+            int escolha;
+
+            do
+            {
+                ExibirMenu();
+                escolha = int.Parse(Console.ReadLine());
+                switch (escolha)
+                {
+                    case 1:
+                        Console.Write("Digite o valor de depósito: ");
+                        decimal valorDeposito = Convert.ToDecimal(Console.ReadLine());
+                        conta.Depositar(valorDeposito);
+                        break;
+                    case 2:
+                        Console.Write("Digite o valor do saque: ");
+                        decimal valorSaque = Convert.ToDecimal(Console.ReadLine());
+                        conta.Sacar(valorSaque);
+                        break;
+                    case 3:
+                        conta.ExibirSaldo();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine($"Bem vindo(a), {conta.Titutlar}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"Obrifgado por usar osistema do POOBank");
+                        break;
+                    default:
+                        Console.WriteLine("opção inválida!");
+                        break;
+                }
+            }
+            while (escolha != 5);
+            
+            Console.ReadKey();
+        }
+
+        public static void ExibirMenu()
+        {
+            Console.WriteLine("Escolha uma operação:");
+            Console.WriteLine("1 - Depositar");
+            Console.WriteLine("2 - Sacar");
+            Console.WriteLine("3 - Exibir Saldo");
+            Console.WriteLine("4 - Reiniciar");
+            Console.WriteLine("5 - Sair");
+         }
+    }
+}
