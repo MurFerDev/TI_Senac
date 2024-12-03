4c192c03551f92c35644607667792a6911838802
Murilo Ferreira <106560525+MurFerDev@users.noreply.github.com>
2024-12-02 22:02:49 -0300

    Exercícios de C#
    Exercícios de C# realizados no Visual Studio para estudo de Programação Orientada a Objetos.

---
+++ Zoologico/Ave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoologico
+{
+    class Ave : Animal
+    {
+        private bool PodeVoar;
+
+        public Ave(string nome, int idade, string alimento, bool podeVoar)
+            : base(nome,idade,alimento) // Chama o construtor da classe base
+        {
+            PodeVoar = podeVoar; // Inicaliza o atributo específico
+        }
+
+        // Sobrescrever o método Descrever para adicionar informações específicas
+        public override string Descrever()
+        {
+            return base.Descrever() + $"Pode voar: {(PodeVoar ? "Sim" : "Não")},";
+        }
+        
+    }
+
+}
+
---
+++ Zoologico/Reptil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoologico
+{
+    class Reptil : Animal
+    {
+        private bool Venenoso; // Atributo específico do reptiliano
+
+        public Reptil(string nome, int idade, string alimento, bool venenoso)
+            : base(nome, idade, alimento)
+        {
+            Venenoso = venenoso;
+        }
+
+        // Sobrescreve o método da classe pai
+        public override string Descrever()
+        {
+            return base.Descrever() + $"É venenoso: {(Venenoso ? "Sim" : "Não")}.";
+        }
+    }
+}
